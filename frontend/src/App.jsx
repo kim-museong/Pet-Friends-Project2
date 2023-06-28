4a72f36a0dad02c470d2245cba3fd261177ee9dc
Eeloo186 <eeloo0186@gmail.com>
2023-06-28 23:22:17 +0900

    init - 2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import MainPage from './pages/MainPage';
 
 const App = () => {
-    return <div>App</div>;
+    return (
+        <Routes>
+            <Route path="/" element={<MainPage />}></Route>
+            <Route path="/auth">
+                <Route path="login" element={<LoginPage />}></Route>
+                <Route path="register" element={<RegisterPage />}></Route>
+            </Route>
+            {/* 게시글 목록 */}
+            {/* 게시글 상세 정보 */}
+        </Routes>
+    );
 };
 
 export default App;
