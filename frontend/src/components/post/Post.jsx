278627f2dffdc38cbd51b5b0d5dd321176449b36
visionx1 <visionx1@hanmail.net>
2023-07-27 17:59:23 +0900

    aaaa

--- frontend/src/components/post/Post.jsx
+++ frontend/src/components/post/Post.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { styled } from 'styled-components';
-import Responsive from '../common/Responsive';
 import { AiOutlineEye, AiOutlineComment, AiFillHeart } from 'react-icons/ai';
 
 const PostBlock = styled.div`
@@ -38,13 +37,12 @@ font-size: 17px;
 `;
 const ThirdBox = styled.div`
 padding: 20px;
-margin-top: 20px;
+margin-top: 10px;
 margin-right: 20px;
 div {
 overflow: hidden;
 display: flex;
 flex-wrap: wrap;
-height: 200px;
 }
 `;
 const StyledSpan = styled.div`
@@ -53,49 +51,42 @@ const StyledSpan = styled.div`
   margin: 10px 20px;     
 `;
 
-const Post = ({ post, likeCount, boardName, loading }) => {
-  if (!post) {
+const Post = ({ post, likeCount, boardName }) => {
+  if( !post ){
     return null;
   }
   const date = new Date(post.post.createdAt);
-  const showDate = `${date.getFullYear()}-${
-    date.getMonth() + 1
-  }-${date.getDate()} ${date.getHours()}:${date.getMinutes()}`;
-
+  const showDate = `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}`
+  
   return (
     <PostBlock>
-      {post && (
-        <>
-          <div>작성자 : {post.post.User.userId}</div>
+      {post && (               
+        <>  
+        <div>
           {boardName === 'community' ? (
-            <div>{`타이틀 : ${post.post.CommunityDetail.title}`}</div>
+            <StyledSpan>{`Title : ${post.post.CommunityDetail.title}`}</StyledSpan>
           ) : boardName === 'information' ? (
-            <div>{`타이틀 : ${post.post.InfoDetail.title}`}</div>
+            <StyledSpan>{`Title : ${post.post.InfoDetail.title}`}</StyledSpan>
           ) : boardName === 'notice' ? (
-            <div>{`타이틀 : ${post.post.NoticeDetail.title}`}</div>
+            <StyledSpan>{`Title : ${post.post.NoticeDetail.title}`}</StyledSpan>
           ) : (
             '존재하지 않는 게시판'
           )}
-          <SecondBox>
-            <div>{post.post.User.userId}</div>
-            <div>{showDate}</div>
-          </SecondBox>
-          <ThirdBox>
-            <div className="test" dangerouslySetInnerHTML={{ __html: post.post.Content.content }} />
-          </ThirdBox>
-          <SecondBox>
-            <div>
-              <AiFillHeart style={{ color: 'rgb(255, 140, 0)' }} /> {likeCount}
-            </div>
-            <div>
-              {' '}
-              <AiOutlineEye style={{ color: 'rgb(255, 140, 0)' }} /> {post.post.view}
-            </div>
-            <div>
-              <AiOutlineComment style={{ color: 'rgb(255, 140, 0)' }} /> {post.commentCount}
-            </div>
-          </SecondBox>
-        </>
+          </div>
+    <SecondBox>
+      <div>{post.post.User.userId}</div>
+      <div>작성일 : {showDate}</div>
+    </SecondBox> 
+    <ThirdBox> 
+      <div className="test" dangerouslySetInnerHTML={{ __html: post.post.Content.content }} />
+    </ThirdBox>
+    <SecondBox>
+      <div></div>
+      <AiFillHeart style={{ color: 'rgb(255, 140, 0)' }}/> {likeCount}
+      <AiOutlineEye style={{ color: 'rgb(255, 140, 0)' }}/> {post.post.view}
+      <AiOutlineComment style={{ color: 'rgb(255, 140, 0)' }}/> {post.commentCount}
+    </SecondBox>         
+          </>  
       )}
     </PostBlock>
   );
